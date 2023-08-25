ad3b762b2ef21a33c5764fd8008327742a0c9c11
chomeed <cmeedeum@gmail.com>
2023-08-25 23:13:34 +0900

    list of dicts

--- utils/remove_duplicates.py
+++ utils/remove_duplicates.py
@@ -1,14 +1,17 @@
-def remove_duplicates_and_keep_highest(predictions):
+def remove_duplicates_and_keep_highest(predictions, image_id=None):
+    '''
+    return : list of dictionaries (keys - image_id, score, bbox, category_id)
+    '''
     unique_predictions = {}
     
-    for score, label, bboxes in predictions:
-        if label in unique_predictions:
-            if score > unique_predictions[label][0]:
-                unique_predictions[label] = (score, bboxes)
+    for score, category_id, bbox in predictions:
+        if category_id in unique_predictions:
+            if score > unique_predictions[category_id][0]:
+                unique_predictions[category_id] = (score, bbox)
         else:
-            unique_predictions[label] = (score, bboxes)
+            unique_predictions[category_id] = (score, bbox)
     
-    filtered_predictions = [(score, label, bboxes) for label, (score, bboxes) in unique_predictions.items()]
+    filtered_predictions = [{"score": score, "category_id": category_id, "bbox": bbox} for category_id, (score, bbox) in unique_predictions.items()]
     
     # Sort the filtered predictions in descending order by score
     filtered_predictions.sort(reverse=True)
--- utils/test_image.py
+++ utils/test_image.py
@@ -1,4 +1,7 @@
 from mmdet.apis import init_detector, inference_detector
+import json 
+
+
 # Load the initialized detection model
 config_file = 'configs/test_tooth_only.py'
 checkpoint_file = 'mmdetection/experiments/tooth_only_2/epoch_20.pth'
@@ -10,18 +13,6 @@ result = inference_detector(model, image)
 #num_classes = model.CLASSES  # Total number of classes in the model
 num_classes = 32
 # Each element of 'result' corresponds to a class
-#result.bboxes
-#result.labels
-#result.scores
-#print(result)
-
-#for k, v in result.pred_instances.items(): 
-#    print(k, v)
-
-#gt_filename = 'mmdetection/data/tooth_detection/annotations/tooth_only_v1/test.json'
-#from mmdet.datasets.api_wrappers import COCO, COCOeval
-#coco_api = COCO(gt_filename)
-
 
 labels = result.pred_instances.labels.tolist()
 scores = result.pred_instances.scores.tolist()
@@ -30,20 +21,11 @@ bboxes = result.pred_instances.bboxes.tolist()
 for bbox, score, label in zip(labels, scores, bboxes):
     print(bbox, score, label)
 
-l = list(zip(scores, labels, bboxes))
+inferences = list(zip(scores, labels, bboxes))
 
 from remove_duplicates import remove_duplicates_highest_score 
 
-l = remove_duplicates_highest_score(l) 
-for _ in l:
+filtered_inferences = remove_duplicates_highest_score(inferences) 
+for _ in filtered_inferences:
     print(_)
 
-
-#for class_id in range(num_classes):    
-#    class_results = result[class_id]  # Detections for the current class
-#    # 'class_results' is an array of shape (N, 5), where N is the number of detections    
-#    # Each row contains [x_min, y_min, x_max, y_max, score] for a detected object
-#    for bbox in class_results:
-#        x_min, y_min, x_max, y_max, score = bbox
-#        #class_name = model.CLASSES[class_id]  # Name of the current class
-#        print(f"Class: {class_id}, BBox: [{x_min}, {y_min}, {x_max}, {y_max}], Score: {score}")
