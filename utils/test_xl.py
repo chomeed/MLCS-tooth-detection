255143bcd5eca409592a1a2af8e0ef7d9f1ed84c
chomeed <cmeedeum@gmail.com>
2023-08-28 11:59:09 +0900

    baseline

--- utils/test_xl.py
+++ utils/test_xl.py
@@ -14,16 +14,34 @@ sheet = write_wb.active
 headers = ['category', 'mAP', 'mAP_50', 'mAP_75', 'mAP_s', 'mAP_m', 'mAP_l']
 sheet.append(headers) 
 
+# Baseline 불러오기 
+# with open('baseline.json', 'r') as rf: 
+#     baseline_data = json.load(rf)
+#     data = list(data.items())
+
 
 with open('test_json.json', 'r') as rf:
-    data = json.load(rf) 
-    sorted_data = sorted(data, key=lambda x: x['category'][-2:])
+    data = json.load(rf)
+    data = list(data.items())
+    sorted_data = sorted(data, key=lambda x: x[0][-2:])
+    print(sorted_data)
 
-for d in sorted_data:
-    new_data = list(d.values())
-    sheet.append(new_data) 
+for row_idx, d in enumerate(sorted_data, start=2):
+    cat = d[0]
+    mAP_data = list(d[1].values())
+    new_data = mAP_data
+    new_data.insert(0, cat)
+    # new data is a list 
 
-# #셀 단위로 추가
-# sheet.cell(5, 5, '5행5열')
+    alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    for col_idx, elem in zip(alphabet, new_data):
+        sheet[f"{col_idx}{row_idx}"] = elem
+    
+
+
+    # sheet.append(new_data) 
+
+# # #셀 단위로 추가
+# # sheet.cell(5, 5, '5행5열')
 
 write_wb.save("test2.xlsx")
